019e804964a0217d08ae8dbbda7beec94c7f1580
AsimAtıcı <asimatici51@gmail.com>
2022-11-02 12:17:24 +0300

    kontrol edit

---
+++ app/Http/Controllers/kontrol.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class kontrol extends Controller
+{
+    public function index() {
+        return "anasayfa  aaa";
+    }
+    public function aboutus() {
+        return "testtttt ";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\kontrol;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homs', function () {
+    return view('home');
+});
+
+Route::get('/anasayfa', function () {
+    return "anasayf aaaa";
+});
+
+Route::get('/hakkimizda', 'kontrol@aboutus');
